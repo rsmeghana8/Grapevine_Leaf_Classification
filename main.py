6b771abfa6f61ec76f05ac8d8688fc2974814836
rsmeghana8 <meghana.reppala@gmail.com>
2023-08-31 13:55:26 -0700

    Prepare callbacks module added

--- main.py
+++ main.py
@@ -24,3 +24,5 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
--- src/CNNClassifier/config/configuration.py
+++ src/CNNClassifier/config/configuration.py
@@ -1,7 +1,7 @@
-
+import os
 from CNNClassifier.constants import *
 from CNNClassifier.utils.common import read_yaml,create_directories
-from CNNClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from CNNClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig , PrepareCallbackConfig
 
 class ConfigurationManager:
     def __init__(
@@ -47,3 +47,21 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+    def get_prepare_callback_config(self)-> PrepareCallbackConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+
+        create_directories([ 
+                            Path(model_ckpt_dir),
+                            Path(config.tensorboard_root_log_dir)  ])
+
+        prepare_callback_config = PrepareCallbackConfig(
+            root_dir = Path(config.root_dir),
+            tensorboard_root_log_dir = Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath= Path(config.checkpoint_model_filepath)
+    
+        )
+
+        return prepare_callback_config
