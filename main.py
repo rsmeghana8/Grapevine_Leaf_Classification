e4cd4540c8ca67b33b30375d125e811f11f0d5f4
rsmeghana8 <meghana.reppala@gmail.com>
2023-08-31 22:21:05 -0700

    Training added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from CNNClassifier import logger
 
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNClassifier.pipeline.stage_02_base_model import PrepareBaseModelTrainingPipeline
+from CNNClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+
 
 STAGE_NAME="Data Ingestion Training"
 try:
@@ -21,6 +23,19 @@ try:
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
     logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<")   
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Training"
+
+try:
+    logger.info(f"***************************************************")
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> Stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/CNNClassifier/config/configuration.py
+++ src/CNNClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 import os
 from CNNClassifier.constants import *
 from CNNClassifier.utils.common import read_yaml,create_directories
-from CNNClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig , PrepareCallbackConfig
+from CNNClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig ,
+                                                PrepareCallbackConfig, TrainingConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -65,3 +66,24 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+
+    def get_training_config(self)-> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model 
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,"Grapevine_Leaves_Image_Dataset")
+        create_directories([Path(training.root_dir)])
+        
+        training__config = TrainingConfig(
+                                root_dir= Path(training.root_dir),
+                                trained_model_path = Path(training.training_model_path),
+                                updated_base_model_path= Path(prepare_base_model.updated_base_model_path),
+                                training_data = Path(training_data),
+                                params_epoch= params.EPOCHS,
+                                params_batch_size = params.BATCH_SIZE,
+                                params_is_augmentation= params.AUGMENTATION,
+                                params_image_size = params.IMAGE_SIZE)
+        
+        return training__config
+    
